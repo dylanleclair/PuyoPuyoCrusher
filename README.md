1835850b71b82e5ce403d8989c28011d00994f7d
dylanleclair <dylan.leclair@icloud.com>
2021-05-11 21:55:18 -0600

    Vastly improved the README
    Also some reformatting.

--- README.md
+++ README.md
@@ -1,8 +1,62 @@
 # PuyoPuyoCrusher
- A new Puyo AI that employs an And-tree based search. 
+
+![](images/initial_server.gif)
 
 
-## To do list :rocket:
+ A revamped Puyo Puyo AI that employs a search as it's main methodology.
 
-- Add formula to check if a maximum has been found :arrows_counterclockwise:
-- Port to another language and use server setup to speed up code :arrows_counterclockwise:
+This has been developed as an HTTP microservice that takes requests about the gamestate of a puyo board, sending back a move to be made using JSON.
+
+# Technologies :hammer:
+
+This project was developed in Rust, with Actix Web as the framework for serving requests. It was designed this was for maximum portability, while also being extremely fast and safely written. 
+
+The Puyo code that is used to send requests to the microservice and visualize it was largely written by my brother, Eric Leclair. 
+
+# Sending and reading responses :dart:
+
+The current state of the codebase is set up to take requests from the AI microservice. 
+
+Any instance of any Puyo game can simply send the AI server a JSON, including the width and height of the board, the board itself, and the buffer of incoming Puyo pieces.
+
+A sample of the JSON that must be sent can be located in the root of this repo, at `sample_board.json`.
+
+Expected output is what move to make, encoded in what I refer to as SPPN or Simple Puyo Puyo Notation:
+
+```json
+{
+   "moves":[
+      [
+         1,
+         2
+      ]
+   ]
+}
+```
+
+This is a 2D array: the inner array will contain arrays of length 2, with the first entry being the column of the board the leftmost Puyo of the piece is to be placed in, with the orientation of the Puyo being denoted in the second, which can be decoded as follows:
+ - 1 = upright
+ - 2 = horizontal (left-to-right)
+ - 3 = downwards
+ - 4 = horizontal (right-to-left)
+
+
+# How to run :car:
+
+To run the code, you will want to clone this repository. 
+
+From there, you can use `cargo` to build the AI microservice: 
+
+- Install Rust / Cargo
+- Navigate to `ai/puyoai`
+- Run `cargo build --release`
+- Run the executable puyoai file in the target directory. 
+
+- In a separate terminal, navigate to `game`
+- Run `python puyovisualizer.py`
+
+Note that not building the release version of the microservice may result in excessively slow operation. 
+
+# To do list :rocket:
+
+- Alter search to make use of a more advanced heuristic, creating more significant chains :arrows_counterclockwise:
--- ai/puyoai/src/ai.rs
+++ ai/puyoai/src/ai.rs
@@ -2,99 +2,93 @@
 use std::collections::VecDeque;
 pub struct Node {
     pub board: Vec<Vec<u8>>,
-    buffer: VecDeque<(u8,u8)>,
+    buffer: VecDeque<(u8, u8)>,
     score_acc: i32,
-    moves: Vec<(u8,u8)> 
+    moves: Vec<(u8, u8)>,
 }
 
 impl Node {
-    pub fn new (
+    pub fn new(
         b: Vec<Vec<u8>>,
-        buf: VecDeque<(u8,u8)>,
+        buf: VecDeque<(u8, u8)>,
         score: i32,
-        moveset: Vec<(u8,u8)>) -> Node {
-            return Node {
-                board:b,
-                buffer: buf,
-                score_acc: score,
-                moves: moveset
-            };
-        }
-    
+        moveset: Vec<(u8, u8)>,
+    ) -> Node {
+        return Node {
+            board: b,
+            buffer: buf,
+            score_acc: score,
+            moves: moveset,
+        };
+    }
     /// Returns a new board, along with the move that was taken to yield it in Simple Puyo Puyo Notation
     /// Tested, runs fine.
-    pub fn div(&self, piece:(u8,u8)) -> Vec<(Vec<Vec<u8>>,(u8,u8))> {
-        let mut new : Vec<(Vec<Vec<u8>>,(u8,u8))> = Vec::new();
+    pub fn div(&self, piece: (u8, u8)) -> Vec<(Vec<Vec<u8>>, (u8, u8))> {
+        let mut new: Vec<(Vec<Vec<u8>>, (u8, u8))> = Vec::new();
         //println!("piece {:?}", piece);
-        let piece = (piece.1,piece.0); // reverse it so encoding works properly
+        let piece = (piece.1, piece.0); // reverse it so encoding works properly
 
         let w = self.board[0].len();
-        if piece.0 == piece.1 
-        {
+        if piece.0 == piece.1 {
             //println!("piece {:?}", piece);
             // if the colors in the piece are the same, we only need to consider 1/2 of all possible moves
             // handle the vertical possibilities
             for i in 0..w {
                 let mut b = self.board.to_vec();
-                super::place(&mut b,piece.0,i as u8);
-                super::place(&mut b, piece.0,i as u8);
-                new.push((b,(i as u8,1)));
+                super::place(&mut b, piece.0, i as u8);
+                super::place(&mut b, piece.0, i as u8);
+                new.push((b, (i as u8, 1)));
             }
 
             // handle the horizontal possibilities
-            for i in 0..w-1 {
+            for i in 0..w - 1 {
                 let mut b = self.board.to_vec();
-                super::place(&mut b,piece.0,i as u8);
-                super::place(&mut b,piece.0,(i+1) as u8);
-                new.push((b,(i as u8,2)));
+                super::place(&mut b, piece.0, i as u8);
+                super::place(&mut b, piece.0, (i + 1) as u8);
+                new.push((b, (i as u8, 2)));
             }
-
         } else {
             for i in 0..w {
                 // handle the vertical possibilities
                 // "top to bottom"
                 let mut b = self.board.to_vec();
-                super::place(&mut b,piece.0,i as u8);
-                super::place(&mut b,piece.1,i as u8);
-                new.push((b,(i as u8,1)));
-                
+                super::place(&mut b, piece.0, i as u8);
+                super::place(&mut b, piece.1, i as u8);
+                new.push((b, (i as u8, 1)));
                 // "bottom to top"
                 let mut b = self.board.to_vec();
-                super::place(&mut b,piece.1,i as u8);
-                super::place(&mut b,piece.0,i as u8);
-                new.push((b,(i as u8,3)));
+                super::place(&mut b, piece.1, i as u8);
+                super::place(&mut b, piece.0, i as u8);
+                new.push((b, (i as u8, 3)));
             }
 
-            for i in 0..w-1 {
+            for i in 0..w - 1 {
                 // handle the horizontal possibilities
-                
                 // "left-to-right"
                 let mut b = self.board.to_vec();
-                super::place(&mut b,piece.0,i as u8);
-                super::place(&mut b,piece.1,(i+1) as u8);
-                new.push((b,(i as u8,2)));
+                super::place(&mut b, piece.0, i as u8);
+                super::place(&mut b, piece.1, (i + 1) as u8);
+                new.push((b, (i as u8, 2)));
                 // "right-to-left"
                 let mut b = self.board.to_vec();
-                super::place(&mut b,piece.1,i as u8);
-                super::place(&mut b,piece.0,(i+1) as u8);
-                
-                new.push((b,(i as u8,4)));
+                super::place(&mut b, piece.1, i as u8);
+                super::place(&mut b, piece.0, (i + 1) as u8);
+
+                new.push((b, (i as u8, 4)));
             }
-            
         }
         return new;
-    } 
+    }
 
     // Advances the search, creating new leaves from a given leaf
     pub fn advance(&self, stack: &mut Vec<Node>) {
-
         if self.buffer.len() == 0 {
             return;
         }
 
         let mut buffer = self.buffer.clone(); // copy the buffer -> this is to be the buffer in the next node
-        let next_piece: (u8,u8) = buffer.pop_front().expect("Buffer is empty."); // get the first move
-        let new_boards =  self.div(next_piece);
+        let next_piece: (u8, u8) = buffer.pop_front().expect("Buffer is empty."); // get the first move
+        let new_boards = self.div(next_piece);
 
         for pair in new_boards {
             let board = pair.0;
@@ -104,41 +98,35 @@ impl Node {
             new_moves.push(next_move); // add the new move to end of accumulated
 
             let results = super::projected_score(board);
-            
             // @TODO shouldn't need to return the board unless you are reconsidering after every move
             // since no side effects are possible using the current heuristic
 
             let new = Node::new(copy, buffer.clone(), self.score_acc + results.1, new_moves);
             stack.push(new);
         }
-
     }
-
 }
-
 
 pub struct Search {
     root: Node,
-    leaves: Vec<Node>
+    leaves: Vec<Node>,
 }
 
 impl Search {
-    pub fn new(board:&Vec<Vec<u8>>, buffer:VecDeque<(u8,u8)>) -> Search {
+    pub fn new(board: &Vec<Vec<u8>>, buffer: VecDeque<(u8, u8)>) -> Search {
         return Search {
             root: Node::new(board.to_vec(), buffer, 0, Vec::new()),
-            leaves: Vec::new()
-        }
-
+            leaves: Vec::new(),
+        };
     }
 
-    pub fn search(&mut self) -> Vec<(u8,u8)> {
+    pub fn search(&mut self) -> Vec<(u8, u8)> {
         // advance the search
         self.root.advance(&mut self.leaves);
         let mut best_score = 0;
         let mut best = self.root.moves.to_vec();
         while self.leaves.len() > 0 {
             let current = self.leaves.pop().expect("Leaf expected, but none found.");
-            
             if current.score_acc > best_score {
                 best_score = current.score_acc;
                 best = current.moves.to_vec();
@@ -147,6 +135,5 @@ impl Search {
             current.advance(&mut self.leaves);
         }
         return best;
-
     }
 }
